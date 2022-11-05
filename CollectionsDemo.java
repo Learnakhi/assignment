c8d11caba3b8fc588e888ac9d27aeb39a0580d42
Learnakhi <115710858+Learnakhi@users.noreply.github.com>
2022-11-05 16:57:04 +0530

    Add files via upload

---
+++ CollectionsDemo.java
@@ -0,0 +1,12 @@
+package com.company;
+import java.util.*;
+public class CollectionsDemo {
+    public static void main(String[] args){
+        List al=new ArrayList();
+        al.add("Akhila");
+        al.add("Bunny");
+        al.add(25);
+        al.add(3.14);
+        System.out.println(al);
+    }
+}
