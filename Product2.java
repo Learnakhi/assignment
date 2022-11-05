4f4368957af90d4c35c8c9ec84e4905041da85ea
Learnakhi <115710858+Learnakhi@users.noreply.github.com>
2022-11-05 16:49:55 +0530

    Add files via upload

---
+++ Product2.java
@@ -0,0 +1,22 @@
+package com.company;
+import java.lang.*;
+import java.util.*;
+
+public class Product2 {
+    public static void main(String[] args)
+    {
+        HashSet<String> obj=new HashSet<>();
+        obj.add("Coins");
+        obj.add("chairs");
+        obj.add("pencils");
+        obj.add("table");
+        obj.add("Books");
+        obj.add("papers");
+        obj.add("carts");
+        obj.add("brushes");
+        obj.add("colors");
+        obj.add("mobiles");
+        System.out.println(obj);
+    }
+}
+
