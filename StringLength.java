33ee2173d704db342bb2ed72952e236768f8952a
Learnakhi <115710858+Learnakhi@users.noreply.github.com>
2022-11-05 17:02:20 +0530

    Add files via upload

---
+++ StringLength.java
@@ -0,0 +1,8 @@
+package com.company;
+import java.util.*;
+public class StringLength {
+    public static void main(String[] args){
+        String s="hellow world";
+        System.out.println(s.length());
+    }
+}
