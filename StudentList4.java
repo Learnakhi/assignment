76ca72625d627c089faa460270d9927269ac8920
Learnakhi <115710858+Learnakhi@users.noreply.github.com>
2022-11-06 23:30:44 +0530

    Add files via upload

---
+++ StudentList4.java
@@ -0,0 +1,27 @@
+package com.company;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
+public class StudentList4
+{
+    public static void main(String[] args)
+    {
+        var path="C:\\Users\\ashana\\Downloads\\Java11Assignments_StudentList.txt";
+
+        try
+        {
+            String data = Files.readString(Path.of(path));
+            System.out.println(data);
+            long numberoflines=data.lines().count();
+            System.out.println(numberoflines) ;
+
+        }catch(IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+
+}
